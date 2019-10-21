00a463dd80758dad10227787f094c0ecfac51f40
Canh Bui <canh.bui@bctech.vn>
2019-10-21 09:12:29 +0700

    update favicon

--- resources/views/backend/master.blade.php
+++ resources/views/backend/master.blade.php
@@ -6,11 +6,10 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
         <link rel="apple-touch-icon" sizes="180x180" href="{!! asset('assets/favicon.ico/apple-touch-icon.png') !!}">
         <link rel="icon" type="image/png" href="{!! asset('assets/favicon.ico/favicon-32x32.png" sizes="32x32') !!}">
         <link rel="icon" type="image/png" href="{!! asset('assets/favicon.ico/favicon-16x16.png" sizes="16x16') !!}">
-        {{--  <link rel="manifest" href="{!! asset('assets/favicon.ico/manifest.json') !!}">  --}}
+        <link rel="manifest" href="{!! asset('assets/favicon.ico/manifest.json') !!}">
         <meta name="theme-color" content="#ffffff">
         <title>@yield('title') | {!! $appName !!}</title>
         <!-- Tell the browser to be responsive to screen width -->
