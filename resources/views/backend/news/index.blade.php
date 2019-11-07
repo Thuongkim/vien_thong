ea80aa4c0510e768c388634d4030b3a364830de8
Kim Thường <thuongbuikim2018@gmail.com>
2019-11-07 17:43:17 +0700

    update frontend

--- resources/views/backend/news/index.blade.php
+++ resources/views/backend/news/index.blade.php
@@ -157,7 +157,7 @@
                             </td>
                             <td style="text-align: center; vertical-align: middle;">{!! $i++ !!}</td>
                             <td style="text-align: justify; vertical-align: middle;">
-                                {!! HTML::link( route('admin.news.index'), \App\Helper\HString::modSubstr($item->title, 50), array('class' => '', 'title' => $item->title)) !!}
+                                {!! HTML::link( route('admin.news.edit', $item->id), \App\Helper\HString::modSubstr($item->title, 50), array('class' => '', 'title' => $item->title)) !!}
                             </td>
                             <td style="text-align: center; vertical-align: middle;">
                                 @if($item->featured == 0)
